82ba59a33f5891c539013b8fba1fc61907ef0702
nyitrairicsi99 <58612330+nyitrairicsi99@users.noreply.github.com>
2021-05-08 22:15:13 +0200

    Initial commit

---
+++ Assets/Scripts/tree.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tree : MonoBehaviour
+{
+    public int hp = 30;
+    public bool cutted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp<=0)
+        {
+
+        }
+    }
+}
